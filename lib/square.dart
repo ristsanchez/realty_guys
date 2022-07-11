f4598dfc0f2fb536ef99409342406440d2bb6b4a
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-10 21:25:22 -0600

    Refactor property class
    The class property represents a game tile that can be owned by a player.
    
    Created getters and setters
    
    These are of different types that come in the form of land, railroad, utility in the form of subclass.

--- lib/square.dart
+++ lib/square.dart
@@ -8,6 +8,48 @@ class Square {
   String name;
   int boardSpace; //id
   String group;
+class Property {
+  final int _id;
+  final int _cost;
+  final String _name;
+  final String _group;
+
+  late Player _owner; //id from player object
+  bool _hasOwner = false;
+  bool _isMortgaged = false;
+
+  Property(
+    this._id,
+    this._name,
+    this._group,
+    this._cost,
+  );
+
+  int get id => _id;
+
+  int get cost => _cost;
+
+  String get name => _name;
+
+  String get group => _group;
+
+  bool get hasOwner => _hasOwner;
+
+  Player get owner => _owner;
+
+  bool get isMortgaged => _isMortgaged;
+
+  set setMortgageStatus(bool condition) {
+    _isMortgaged = condition;
+  }
+
+  set setIsOwned(bool condition) {
+    _hasOwner = condition;
+  }
+
+  set setOwner(Player player) {
+    _owner = player;
+  }
 }
 
 class Property extends Square {
