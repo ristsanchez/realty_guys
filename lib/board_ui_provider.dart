b4f0c636f5511b162acc8496cf52efccbea9f94e
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-10 21:38:08 -0600

    Update provider parameters
    Minor update to parameters and several methods

--- lib/board_ui_provider.dart
+++ lib/board_ui_provider.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/foundation.dart';
 
 class BoardProvider extends ChangeNotifier {
-  bool tempButton;
+  int _currentTile;
   bool _showCosts;
+  bool _showOwner;
 
-  bool get temp => tempButton;
-
-  bool get costs => _showCosts;
+  int get index => _currentTile;
+  bool get costVisibility => _showCosts;
+  bool get ownerVisibility => _showOwner;
 
   BoardProvider()
       : _showCosts = false,
-        tempButton = true;
+        _showOwner = false,
+        _currentTile = -1;
+
+  void increase() {
+    _currentTile++;
+    _currentTile %= 40;
+    notifyListeners();
+  }
 
   void toggleCostsVisibility() {
     _showCosts = !_showCosts;
     notifyListeners();
   }
 
-  void setTemp() {
-    tempButton = !tempButton;
+  void toggleOwnerVisibility() {
+    _showOwner = !_showOwner;
     notifyListeners();
   }
 }
