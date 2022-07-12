46fafc8a40f159c38b5c4c9b4f412a6784587b15
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-11 23:44:39 -0600

    Create selection provider
    Selection provider for player icon to be displayed in game.

---
+++ lib/selection_provider.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:realty_guys/color_constants.dart';
+
+class SelectionMenuProvider extends ChangeNotifier {
+  final _colorList = colorList;
+  final _iconList = iconList;
+  int _colorIndex;
+  int _index;
+
+  SelectionMenuProvider()
+      : _colorIndex = 0,
+        _index = 0;
+
+  Icon get currentIcon => Icon(
+        _iconList.elementAt(_index),
+        size: 40, //change this
+        color: _colorList.elementAt(_colorIndex),
+      );
+
+  void randomize() {
+    _index = Random().nextInt(_iconList.length);
+    _colorIndex = Random().nextInt(_colorList.length);
+    notifyListeners();
+  }
+
+  void nextIcon() {
+    _index++;
+    if (_index >= _iconList.length) _index = 0;
+    notifyListeners();
+  }
+
+  void previousIcon() {
+    _index--;
+    if (_index < 0) _index = _iconList.length - 1;
+    notifyListeners();
+  }
+
+  void changeColor() {
+    _colorIndex++;
+    if (_colorIndex > _colorList.length - 1) _colorIndex = 0;
+    notifyListeners();
+  }
+}
