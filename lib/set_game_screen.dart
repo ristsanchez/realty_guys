0a27bf4b867ac1372b4427aebb8d496476126924
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-11 23:43:56 -0600

    Add widgets to setup screen
    Added little icon menu to select player icon and color. Fully functional with provider.
    
    Also added text field where user will input their name, their in game name.

--- lib/set_game_screen.dart
+++ lib/set_game_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:realty_guys/board.dart';
 import 'package:realty_guys/board_screen.dart';
 import 'package:realty_guys/board_ui_provider.dart';
+import 'package:realty_guys/selection_provider.dart';
 
 class SetGameScreen extends StatelessWidget {
   const SetGameScreen({Key? key}) : super(key: key);
@@ -9,12 +11,45 @@ class SetGameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         toolbarHeight: 0,
       ),
+      body: mainBody(context),
     );
   }
 }
+
+mainBody(BuildContext context) {
+  Icon currentIcon =
+      Provider.of<SelectionMenuProvider>(context, listen: true).currentIcon;
+  const double opacity = 0.5;
+  return Center(
+    child: Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Colors.white10,
+      ),
+      width: MediaQuery.of(context).size.width * 0.8,
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+              child: SizedBox(
+                height: 30,
+                child: TextField(
+                  textAlignVertical: TextAlignVertical.bottom,
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: [
                 Expanded(
                   flex: 1,
                   child: Padding(
@@ -43,3 +78,69 @@ class SetGameScreen extends StatelessWidget {
                     ),
                   ),
                 ),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      Opacity(
+                        opacity: opacity,
+                        child: IconButton(
+                            onPressed: () {
+                              Provider.of<SelectionMenuProvider>(context,
+                                      listen: false)
+                                  .randomize();
+                            },
+                            icon: const Icon(Icons.casino)),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Opacity(
+                            opacity: opacity,
+                            child: IconButton(
+                              onPressed: () {
+                                Provider.of<SelectionMenuProvider>(context,
+                                        listen: false)
+                                    .previousIcon();
+                              },
+                              icon: const Icon(Icons.skip_previous_rounded),
+                            ),
+                          ),
+                          Padding(
+                              padding: const EdgeInsets.all(0),
+                              child: currentIcon),
+                          Opacity(
+                            opacity: opacity,
+                            child: IconButton(
+                              onPressed: () {
+                                Provider.of<SelectionMenuProvider>(context,
+                                        listen: false)
+                                    .nextIcon();
+                              },
+                              icon: const Icon(Icons.skip_next_rounded),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Opacity(
+                        opacity: opacity,
+                        child: IconButton(
+                            onPressed: () {
+                              Provider.of<SelectionMenuProvider>(context,
+                                      listen: false)
+                                  .changeColor();
+                            },
+                            icon: const Icon(Icons.color_lens_rounded)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
