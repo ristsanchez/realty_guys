58e5b5a246f88d07a5a028617ed5d2016fef9c6e
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-11 23:48:10 -0600

    Update board provider
    Changed class name.
    
    Added quarter turns variable to control board orientation.

--- lib/board_ui_provider.dart
+++ lib/board_ui_provider.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/foundation.dart';
 
-class BoardProvider extends ChangeNotifier {
+class BoardUIProvider extends ChangeNotifier {
   int _currentTile;
   bool _showCosts;
   bool _showOwner;
+  int _quarterTurns;
 
   int get index => _currentTile;
   bool get costVisibility => _showCosts;
   bool get ownerVisibility => _showOwner;
 
-  BoardProvider()
+  int get rotations => _quarterTurns;
+
+  BoardUIProvider()
       : _showCosts = false,
         _showOwner = false,
-        _currentTile = -1;
+        _currentTile = -1,
+        _quarterTurns = 0;
 
   void increase() {
     _currentTile++;
@@ -27,6 +31,12 @@ class BoardProvider extends ChangeNotifier {
 
   void toggleOwnerVisibility() {
     _showOwner = !_showOwner;
+    notifyListeners();
+  }
+
+  void rotateBoard() {
+    _quarterTurns++;
+    if (_quarterTurns > 3) _quarterTurns = 0;
     notifyListeners();
   }
 }
