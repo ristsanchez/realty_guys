808998b267fe746aef77e74a072cac88d329217a
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-12 22:42:46 -0600

    Update color constants

--- lib/color_constants.dart
+++ lib/color_constants.dart
@@ -12,17 +12,17 @@ final Color darkBlue = Colors.blue.shade700.withOpacity(0.4);
 
 final Color green = Colors.greenAccent.shade200.withOpacity(0.6);
 
-final Color yellow = Colors.yellow.shade300.withOpacity(0.5);
+final Color yellow = Colors.yellow.withOpacity(0.5);
 
 final Color red = Colors.red.shade400.withOpacity(0.5);
 
-final Color orange = Colors.orange.shade400.withOpacity(0.5);
+final Color orange = Colors.orangeAccent.shade400.withOpacity(0.5);
 
 final Color pink = Colors.purpleAccent.shade200.withOpacity(0.5);
 
 final Color lightBlue = Colors.lightBlue.shade200.withOpacity(0.7);
 
-final Color brown = Colors.brown.shade300.withOpacity(0.5);
+final Color brown = Colors.brown.shade300.withOpacity(0.6);
 
 final List<IconData> iconList = [
   Icons.skateboarding,
