c5518f161bc8b42b018558f0c683926ec2bceac0
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-13 23:14:48 -0600

    Update board player positions type
    Changed from a hashmap of player and int to hashmap of int and int.
    
    This is better since the board class shouldn't care about player properties other that player id.
    
    This way only the necessary data is passed through/to/from the class methods.

--- lib/board.dart
+++ lib/board.dart
@@ -4,58 +4,53 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:realty_guys/player.dart';
 
-class Board extends ChangeNotifier {
-  late HashMap<Player, int> _playerPositions; //don't make final
+const int jail = 10;
 
-  Board();
+class Board extends ChangeNotifier {
+  // player id and then its position
+  late HashMap<int, int> _playerPositions;
+
+  //don't make final
+
+  Board() : _playerPositions = HashMap();
+
+  HashMap<int, int> get getPositions => _playerPositions;
+
+  int getPlayerPosition(int id) => _playerPositions[id] ?? -1;
+
+  void setPositions(HashMap<int, int> positions) {
+    _playerPositions = positions;
+  }
 
   void initialize(Set<Player> playerSet) {
-    //init player hashmap
-    _playerPositions = HashMap();
-
     for (Player player in playerSet) {
-      _playerPositions.putIfAbsent(player, () => 0);
+      _playerPositions.putIfAbsent(player.id, () => 0);
     }
+    notifyListeners();
+    print('initialized player positions');
   }
 
-  void addPlayer(Player player) {
-    if (_playerPositions.containsKey(player)) {
-      // throw new IllegalStateException("Player has already joined the game.");
-    } else {
-      _playerPositions.putIfAbsent(player, () => 0);
-    }
+  void sendToJail(int playerId) {
+    _playerPositions.update(playerId, (value) => jail);
+    //notify landing?
+    notifyListeners();
   }
 
-  int getPosition(Player player) {
-    return _playerPositions[player] ?? -1;
-  }
+  void advance(int playerId, int numberOfSpaces) {
+    int currentPosition = _playerPositions[playerId] ?? -1;
+    int newPosition = (numberOfSpaces + currentPosition) % 40;
 
-  HashMap<Icon, int> getAllPositions() {
-    HashMap<Icon, int> temp = HashMap();
-
-    _playerPositions.forEach((key, value) {
-      temp.putIfAbsent(
-          Icon(key.playerIconData, color: key.playerColor), () => value);
-    });
-
-    return temp;
-  }
-
-  void advance(Player player, int numberOfSpaces) {
-    int currentPosition = _playerPositions[player] ?? -1;
     if (currentPosition < 0) {
       // throw new IllegalArgumentException("Player does not play in the game.");
     } else if (numberOfSpaces <= 0) {
       // throw new IllegalArgumentException("numberOfFields must be positive: " + numberOfFields);
     } else {
-      _playerPositions.update(
-          player, (numberOfSpaces) => (numberOfSpaces + currentPosition));
+      _playerPositions.update(playerId, (value) => newPosition);
       notifyListeners();
       //provider positions?
       //show movement animation?
-
     }
 
-    //behavior for landing separate?
+    //behavior for landing separate? A: yes
   }
 }
