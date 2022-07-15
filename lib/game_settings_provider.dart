9227eb36e3ead69cbc30113fec23758809922086
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-14 23:53:30 -0600

    Add class fields
     to get data before game init

--- lib/game_settings_provider.dart
+++ lib/game_settings_provider.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
 import 'dart:collection';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:realty_guys/color_constants.dart';
 import 'package:realty_guys/json_util_data.dart';
+import 'package:realty_guys/luck_card.dart';
+import 'package:realty_guys/timer_local.dart';
 
 class GameSettings extends ChangeNotifier {
   final _colorList = colorList;
@@ -11,32 +14,46 @@ class GameSettings extends ChangeNotifier {
   int _colorIndex;
   int _index;
 
-  bool _started;
-  late HashMap<int, dynamic> _data;
+  bool _isDataReady;
+  late List<LuckCard> _chanceCards;
+  late List<LuckCard> _luckyCards;
+
+  late HashMap<int, dynamic> _tileData;
 
   GameSettings()
       : _colorIndex = 0,
         _index = 0,
-        _started = false,
-        _data = HashMap();
+        _isDataReady = false,
+        _tileData = HashMap(),
+        _chanceCards = [],
+        _luckyCards = [];
 
-  bool get isInit => _started;
+  bool get isInit => _isDataReady;
 
-  HashMap<int, dynamic> get data => _data;
+  List<LuckCard> get chanceCards => _chanceCards;
 
-  Icon get currentIcon => Icon(
-        _iconList.elementAt(_index),
-        size: 40, //change this
-        color: _colorList.elementAt(_colorIndex),
-      );
+  List<LuckCard> get luckyCards => _chanceCards;
+
+  HashMap<int, dynamic> get tileData => _tileData;
 
   IconData get selectedIconData => _iconList.elementAt(_index);
   Color get selectedColor => _colorList.elementAt(_colorIndex);
 
+  Icon get currentIcon => Icon(
+        _iconList.elementAt(_index),
+
+        //This is fine, maybe because its just for display, not for actual game
+        size: 40,
+        color: _colorList.elementAt(_colorIndex),
+      );
+
   void init() async {
-    _data = await getPropertyData();
-    await Future.delayed(const Duration(milliseconds: 500));
-    _started = true;
+    _tileData = await getPropertyData();
+    _chanceCards = await getLuckyCardsData('chance');
+    _luckyCards = await getLuckyCardsData('lucky');
+    await Future.delayed(const Duration(milliseconds: 250));
+
+    _isDataReady = true;
     notifyListeners();
   }
 
