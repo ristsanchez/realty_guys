3f5bc111a502c2223c7c2661dcb26ff007606aad
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-18 22:36:34 -0600

    refactor tile data into list

--- lib/utils/json_utils.dart
+++ lib/utils/json_utils.dart
@@ -1,34 +1,34 @@
 import 'dart:collection';
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:realty_guys/models/luck_card.dart';
+import 'package:realty_guys/models/player.dart';
 import 'package:realty_guys/models/tile.dart';
 
 abstract class JsonUtils {
   JsonUtils._();
 
-  static Future<HashMap<int, dynamic>> getPropertyData() async {
+  static Future<List> getPropertyData() async {
     String data = await rootBundle.loadString('lib/assets/tile_data.json');
     Map<String, dynamic> map = json.decode(data);
-
-    HashMap<int, dynamic> boardSpaces = HashMap();
     List anotherTemp = map['properties'];
+
+    List boardSpaces = [];
 
     for (var element in anotherTemp) {
       if (element['group'] == 'special') {
-        boardSpaces.putIfAbsent(
-            element['id'], () => SpecialTile.fromJson(element));
+        boardSpaces.add(SpecialTile.fromJson(element));
       } else if (element['group'] == 'railroad') {
-        boardSpaces.putIfAbsent(
-            element['id'], () => Railroad.fromJson(element));
+        boardSpaces.add(Railroad.fromJson(element));
       } else if (element['group'] == 'utility') {
-        boardSpaces.putIfAbsent(element['id'], () => Utility.fromJson(element));
-      } else {
-        //Properties colored/lands/the ones with houses
-        boardSpaces.putIfAbsent(element['id'], () => Land.fromJson(element));
+        boardSpaces.add(Utility.fromJson(element));
+      } else /*Properties colored/lands/the ones with houses*/ {
+        boardSpaces.add(Land.fromJson(element));
       }
     }
+    boardSpaces.sort(((a, b) => a.id.compareTo(b.id)));
     return boardSpaces;
   }
 
@@ -54,5 +54,29 @@ abstract class JsonUtils {
     //this way the actual card data is not passed through the network,
     //and client only needs to initialize card with title and id
     //or maybe not
+  }
+
+  ///temp method removed later
+  static Future<HashSet<Player>> tempPlayers() {
+    //init json data? after first build
+    HashSet<Player> players = HashSet();
+
+    players.add(Player(
+        id: 0, name: 'Yoko', color: Colors.red, iconData: Icons.no_accounts));
+    players.add(Player(
+        id: 1,
+        name: 'John',
+        color: Colors.greenAccent.shade700,
+        iconData: Icons.piano));
+
+    players.add(Player(
+        id: 2,
+        name: 'Lennon',
+        color: Colors.orangeAccent.shade700,
+        iconData: Icons.insert_emoticon));
+
+    return Future.delayed(const Duration(seconds: 4), () {
+      return players;
+    });
   }
 }
