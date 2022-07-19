3534dc1cc998549fadd312a770ba3936ad8697bf
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-18 22:38:08 -0600

    create ring stack view

---
+++ lib/presentation/board_components/center_components/inner_ring_stack.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:realty_guys/presentation/board_components/center_components/possible_landings.dart';
+import 'package:realty_guys/presentation/board_components/center_components/property_owners.dart';
+import 'package:realty_guys/providers/board_ui_provider.dart';
+
+class InnerRingStack extends StatelessWidget {
+  const InnerRingStack({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<BoardUIProvider>(
+      builder: ((context, boardUIProvider, child) {
+        bool ifShowRolls = boardUIProvider.costVisibility;
+        bool ifShowOwners = true;
+
+        //Stack places widgets from back->front of the screen
+        return Stack(
+          children: [
+            Visibility(visible: ifShowOwners, child: const PropertyOwners()),
+            Visibility(visible: ifShowRolls, child: const RingRolls()),
+            //more settings here
+          ],
+        );
+      }),
+    );
+  }
+}
