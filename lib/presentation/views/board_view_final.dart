3eb2b3a0762690a1036f379570387c599f61472c
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2022-07-19 23:53:39 -0600

    Update providers and layout

--- lib/presentation/views/board_view_final.dart
+++ lib/presentation/views/board_view_final.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:realty_guys/dialogs/roll_dialog.dart';
 import 'package:realty_guys/presentation/board_components/actions_bar.dart';
 import 'package:realty_guys/presentation/board_components/activated_actions_bar.dart';
 import 'package:realty_guys/presentation/board_components/board_foundation.dart';
@@ -23,89 +24,92 @@ class BoardLayout extends StatelessWidget {
     final double maxX = MediaQuery.of(context).size.width;
     final double maxY = MediaQuery.of(context).size.height;
 
-    return Consumer<GameController>(
-      builder: (context, gameController, child) {
-        bool ifGameIsRunning = gameController.isGameGoing;
-        double timeLeft = gameController.timeLeft / 10;
-        return Column(
-          children: [
-            Visibility(
-              visible: ifGameIsRunning,
-              child: LinearProgressIndicator(
-                semanticsLabel: 'Time left in turn',
-                value: timeLeft,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(10, ifGameIsRunning ? 0 : 4, 10, 10),
-              child: MultiProvider(
-                providers: [
-                  ChangeNotifierProvider<BoardUIProvider>(
-                      create: (_) => BoardUIProvider()),
-                  ChangeNotifierProvider(create: (_) => gameController.board),
-                ],
-                child: Column(
-                  children: [
-                    const BoardTopBar(),
-
-                    //Move this to a separate class called boardStack
-                    Selector<BoardUIProvider, int>(
-                        selector: (_, boardUI) => boardUI.rotations,
-                        builder: (context, rotations, child) {
-                          return RotatedBox(
-                            quarterTurns: rotations,
-                            child: Container(
-                              color: AppColors.backBoard,
-                              //make this dimensions constants
-                              width: maxX - 20,
-                              height: maxX - 20,
-                              //Whole board as a Row
-                              child: Stack(
-                                children: const [
-                                  BoardFoundation(), //board background
-                                  //from getBoard(context, propertyData, rotations),
-
-                                  PlayersLayer(),
-                                  //this moved ^_playerPiecesOnBoard(context, 4 - rotations),
-                                  MovementLayer(), //tbi
-                                  //moved this ^ to a single method below lightUpTile(context, maxX - 20),
-
-                                  //maybe the tile info layer on top of everything
-                                  // so user can click without touching pieces
-                                ],
-                              ),
-                            ),
-                          );
-                        }),
-                    const Divider(
-                      height: 2,
-                    ),
-                    Visibility(
-                      visible: ifGameIsRunning,
-                      child: const BoardActionsBar(),
-                    ),
-                    const Divider(height: 2),
-                    const PlayerInfoBar(),
-                    const Divider(height: 2),
-                    const PropertyAction(),
-                    Visibility(
-                      visible: !ifGameIsRunning,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          //init game
-                          Provider.of<GameController>(context, listen: false)
-                              .startGame();
-                        },
-                        child: const Text('Start game'),
-                      ),
-                    ),
-                  ],
+    return Scaffold(
+      // backgroundColor: AppColors.baseGray,
+      body: Consumer<GameController>(
+        builder: (context, gameController, child) {
+          return Column(
+            children: [
+              Visibility(
+                visible: gameController.isGameGoing,
+                replacement: const SizedBox(height: 4),
+                child: LinearProgressIndicator(
+                  value: gameController.timeLeft / 30,
                 ),
               ),
-            ),
-          ],
-        );
-      },
+              Container(
+                padding: EdgeInsets.fromLTRB(
+                    10, gameController.isGameGoing ? 0 : 4, 10, 10),
+                child: MultiProvider(
+                  providers: [
+                    ChangeNotifierProvider<BoardUIProvider>(
+                        create: (_) => BoardUIProvider()),
+                    ChangeNotifierProvider(create: (_) => gameController.board),
+                  ],
+                  child: Column(
+                    children: [
+                      const BoardTopBar(),
+                      //Move this to a separate class called boardStack
+                      Selector<BoardUIProvider, int>(
+                          selector: (_, boardUI) => boardUI.rotations,
+                          builder: (context, rotations, child) {
+                            return Container(
+                              color: AppColors.backBoard,
+                              //make this dimension constant
+                              //width stays constant
+                              height: maxX - 20,
+                              child: RotatedBox(
+                                quarterTurns: rotations,
+                                child: Stack(
+                                  children: const [
+                                    BoardFoundation(), //board background
+                                    //from getBoard(context, propertyData, rotations),
+
+                                    PlayersLayer(),
+                                    //this moved ^_playerPiecesOnBoard(context, 4 - rotations),
+                                    MovementLayer(), //tbi
+                                    //moved this ^ to a single method below lightUpTile(context, maxX - 20),
+
+                                    //maybe the tile info layer on top of everything
+                                    // so user can click without touching pieces
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                      const Divider(
+                        height: 2,
+                      ),
+                      Visibility(
+                        visible: gameController.isGameGoing,
+                        child: const BoardActionsBar(),
+                      ),
+                      const Divider(height: 2),
+                      Visibility(
+                          visible: gameController.isGameGoing,
+                          child: const PlayerInfoBar()),
+                      const Divider(height: 2),
+                      const PropertyAction(), //visible only if
+                      Visibility(
+                        visible: !gameController.isGameGoing,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            //init game
+                            // Provider.of<GameController>(context, listen: false)
+                            //     .startGame();
+                            showRollDialog(context);
+                          },
+                          child: const Text('Start game'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
